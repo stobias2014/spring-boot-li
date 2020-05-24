b6d55f7cf32be9b6a66b0b6aa9ff807f39a608a7
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-24 13:53:44 -0500

    added Rest Room Controller

---
+++ learning-spring/src/main/java/com/tobias/saul/learningspring/web/controller/RoomController.java
@@ -0,0 +1,29 @@
+package com.tobias.saul.learningspring.web.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tobias.saul.learningspring.data.entity.Room;
+import com.tobias.saul.learningspring.data.repository.RoomRepository;
+
+@RestController
+@RequestMapping("/api/v1")
+public class RoomController {
+	
+	private final RoomRepository roomRepository;
+	
+	@Autowired
+	public RoomController(RoomRepository roomRepository) {
+		this.roomRepository = roomRepository;
+	}
+	
+	@GetMapping("/rooms")
+	public List<Room> getAllRooms() {
+		return roomRepository.findAll();
+	}
+
+}
