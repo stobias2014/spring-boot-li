afd6dfd8a5a30d5d44d2b92b4422f807368316a8
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-24 17:02:57 -0500

    added RoomReservationWebController

--- learning-spring/src/main/java/com/tobias/saul/learningspring/web/controller/RoomReservationWebController.java
+++ learning-spring/src/main/java/com/tobias/saul/learningspring/web/controller/RoomReservationWebController.java
@@ -1,9 +1,20 @@
 package com.tobias.saul.learningspring.web.controller;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.tobias.saul.learningspring.business.domain.RoomReservation;
 import com.tobias.saul.learningspring.business.service.ReservationService;
+import com.tobias.saul.learningspring.web.util.DateUtils;
 
+@Controller
+@RequestMapping("reservations")
 public class RoomReservationWebController {
 	
 	private final ReservationService reservationService;
@@ -12,9 +23,15 @@ public class RoomReservationWebController {
 		this.reservationService = reservationService;
 	}
 	
-	public String roomReservationsView(Model model) {
+	@GetMapping("/{date}")
+	public String roomReservationsView(Model model, @PathVariable("date") String dateString) {
 		
-		return "roomreservations";
+		Date date = DateUtils.createDateFromDateString(dateString);
+		
+		List<RoomReservation> roomReservations = reservationService.getRoomReservationsForDate(date);
+		model.addAttribute("roomReservations", roomReservations);
+		
+		return "reservations";
 	}
 
 }
