20b18f3e02b041492200449451a808cbda6cf8ca
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-24 19:49:58 -0500

    added guest services

---
+++ guest-services/src/main/java/com/tobias/saul/guestservices/GuestServicesApplication.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.guestservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuestServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuestServicesApplication.class, args);
+	}
+
+}
