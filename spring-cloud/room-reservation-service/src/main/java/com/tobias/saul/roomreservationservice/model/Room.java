765d318cb9175401d6fd41ffee242d0c2e0ab6e1
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-25 15:14:57 -0500

    added RoomReservationWebService to use RoomServices

---
+++ spring-cloud/room-reservation-service/src/main/java/com/tobias/saul/roomreservationservice/model/Room.java
@@ -0,0 +1,36 @@
+package com.tobias.saul.roomreservationservice.model;
+
+public class Room {
+	
+	private Long roomId;
+	private String name;
+	private String roomNumber;
+	private String bedInfo;
+	public Long getRoomId() {
+		return roomId;
+	}
+	public void setRoomId(Long roomId) {
+		this.roomId = roomId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getRoomNumber() {
+		return roomNumber;
+	}
+	public void setRoomNumber(String roomNumber) {
+		this.roomNumber = roomNumber;
+	}
+	public String getBedInfo() {
+		return bedInfo;
+	}
+	public void setBedInfo(String bedInfo) {
+		this.bedInfo = bedInfo;
+	}
+	
+	
+
+}
---
+++ spring-cloud/room-reservation-service/src/main/java/com/tobias/saul/roomreservationservice/web/service/RoomReservationWebService.java
@@ -0,0 +1,47 @@
+package com.tobias.saul.roomreservationservice.web.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.tobias.saul.roomreservationservice.dto.RoomReservation;
+import com.tobias.saul.roomreservationservice.model.Room;
+
+@RestController
+@RequestMapping("/api/v1/room-reservations")
+public class RoomReservationWebService {
+	
+	private final RestTemplate restTemplate;
+	
+	public RoomReservationWebService(RestTemplate restTemplate) {
+		this.restTemplate = restTemplate;
+	}
+	
+	@GetMapping
+	public List<RoomReservation> getRoomReservations() {
+		List<Room> rooms = getAllRooms();
+		List<RoomReservation> roomReservations = new ArrayList<RoomReservation>();
+		rooms.forEach(room -> {
+			RoomReservation roomReservation = new RoomReservation();
+			roomReservation.setRoomId(room.getRoomId());
+			roomReservation.setRoomName(room.getName());
+			roomReservation.setRoomNumber(room.getRoomNumber());
+			roomReservations.add(roomReservation);
+		});
+		return roomReservations;
+	}
+	
+	public List<Room> getAllRooms() {
+		ResponseEntity<List<Room>> roomResponse = restTemplate.exchange("http://ROOMSERVICES/rooms",
+				HttpMethod.GET, null, new ParameterizedTypeReference<List<Room>>() {});
+		return roomResponse.getBody();
+	}
+
+}
