024d1f8f19be0ef9d9f3bab3bbfd38b2f8f828a2
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-25 12:34:45 -0500

    added room-servies project

---
+++ spring-cloud/room-services/src/test/java/com/tobias/saul/roomservices/RoomServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.roomservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
