5fd1b7864636a913d4a4d3f41691959d2bffe4f6
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-25 12:44:42 -0500

    added RoomWebService

--- spring-cloud/room-services/src/main/java/com/tobias/saul/roomservices/web/services/RoomWebService.java
+++ spring-cloud/room-services/src/main/java/com/tobias/saul/roomservices/web/services/RoomWebService.java
@@ -1,8 +1,13 @@
 package com.tobias.saul.roomservices.web.services;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tobias.saul.roomservices.entity.Room;
 import com.tobias.saul.roomservices.repository.RoomRepository;
 
 @RestController
@@ -15,6 +20,13 @@ public class RoomWebService {
 		this.roomRepository = roomRepository;
 	}
 	
+	@GetMapping
+	public List<Room> getAllRooms() {
+		return roomRepository.findAll();
+	}
 	
-
+	@GetMapping("/{roomId}")
+	public Room getRoom(@PathVariable("roomId") Long roomId) {
+		return roomRepository.findById(roomId).get();
+	}
 }
