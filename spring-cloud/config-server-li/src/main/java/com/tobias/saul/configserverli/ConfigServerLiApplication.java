40f1eea2f63bd8bb487f9e83807729ded4349b98
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-25 13:26:36 -0500

    added config server project

---
+++ spring-cloud/config-server-li/src/main/java/com/tobias/saul/configserverli/ConfigServerLiApplication.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.configserverli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerLiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerLiApplication.class, args);
+	}
+
+}
---
+++ spring-cloud/config-server-li/src/test/java/com/tobias/saul/configserverli/ConfigServerLiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.configserverli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerLiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
