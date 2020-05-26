408393be7798b5ab7cc37ea04a6b5f6cb6e46b92
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-26 14:44:22 -0500

    added explore-cali project

---
+++ explore-cali/src/main/java/com/tobias/saul/explorecali/ExploreCaliApplication.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.explorecali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreCaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreCaliApplication.class, args);
+	}
+
+}
---
+++ explore-cali/src/test/java/com/tobias/saul/explorecali/ExploreCaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.explorecali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExploreCaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
