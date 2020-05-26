c7663d9603a0ba704157975cee7f249b2854798b
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-26 18:46:10 -0500

    updated repositories to REST resources

--- explore-cali/src/main/java/com/tobias/saul/explorecali/repository/TourPackageRepository.java
+++ explore-cali/src/main/java/com/tobias/saul/explorecali/repository/TourPackageRepository.java
@@ -1,13 +1,37 @@
 package com.tobias.saul.explorecali.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import com.tobias.saul.explorecali.domain.TourPackage;
 
-@Repository
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourPackageRepository extends JpaRepository<TourPackage, String>{
 
 	TourPackage findByName(String tourPackageName);
+
+	@Override
+	@RestResource(exported = false)
+	<S extends TourPackage> S save(S entity); 
+
+	@Override
+	@RestResource(exported = false)
+	void deleteById(String id);
+
+	@Override
+	@RestResource(exported = false)
+	void delete(TourPackage entity);
+	
+	@Override
+	@RestResource(exported = false)
+	void deleteAll(Iterable<? extends TourPackage> entities); 
+
+	@Override
+	@RestResource(exported = false)
+	void deleteAll();
+	
+	
 
 }
--- explore-cali/src/main/java/com/tobias/saul/explorecali/repository/TourRepository.java
+++ explore-cali/src/main/java/com/tobias/saul/explorecali/repository/TourRepository.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Repository;
 
 import com.tobias.saul.explorecali.domain.Tour;
@@ -16,4 +17,28 @@ public interface TourRepository extends PagingAndSortingRepository<Tour, Long> {
 	
 	Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 
+	@Override
+	@RestResource(exported = false)
+	<S extends Tour> S save(S entity);
+
+	@Override
+	@RestResource(exported = false)
+	<S extends Tour> Iterable<S> saveAll(Iterable<S> entities);
+
+	@Override
+	@RestResource(exported = false)
+	void deleteById(Long id);
+
+	@Override
+	@RestResource(exported = false)
+	void delete(Tour entity);
+
+	@Override
+	@RestResource(exported = false)
+	void deleteAll(Iterable<? extends Tour> entities);
+
+	@Override
+	@RestResource(exported = false)
+	void deleteAll();
+	
 }
