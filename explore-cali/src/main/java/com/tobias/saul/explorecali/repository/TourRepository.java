f1a976c7a24332eda8a96a4084b8a67c91de3d48
Saul Tobias <saul.tobias@fdmgroup.com>
2020-05-26 18:35:34 -0500

    updated to extend Pageable repo

--- explore-cali/src/main/java/com/tobias/saul/explorecali/repository/TourRepository.java
+++ explore-cali/src/main/java/com/tobias/saul/explorecali/repository/TourRepository.java
@@ -2,15 +2,18 @@ package com.tobias.saul.explorecali.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.tobias.saul.explorecali.domain.Tour;
 
 @Repository
-public interface TourRepository extends JpaRepository<Tour, Long> {
+public interface TourRepository extends PagingAndSortingRepository<Tour, Long> {
 	
-	List<Tour> findByTourPackageCode(@Param("code") String code);
+	Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 
 }
