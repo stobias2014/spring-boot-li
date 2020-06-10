97075b8ee6ea7103075da883c1793035ef625f7e
Saul Tobias <saul.tobias@fdmgroup.com>
2020-06-10 16:43:22 -0500

    added spring mvc

---
+++ spring-mvc-li/src/main/java/com/tobias/saul/springmvcli/SpringMvcLiApplication.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.springmvcli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcLiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcLiApplication.class, args);
+	}
+
+}
---
+++ spring-mvc-li/src/test/java/com/tobias/saul/springmvcli/SpringMvcLiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobias.saul.springmvcli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcLiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
